36fd16f19b03a9caaedd42d0c3cc66c7250a8e4f
Piyush <piyushat25@gmail.com>
2024-09-15 19:48:00 +0530

    added zod for mobile phone validation

--- src/app/login/otp.tsx
+++ src/app/login/otp.tsx
@@ -30,6 +30,7 @@ const FormSchema = z.object({
 
 export default function InputOTPForm() {
     const router = useRouter()
+
     const form = useForm<z.infer<typeof FormSchema>>({
         resolver: zodResolver(FormSchema),
         defaultValues: {
